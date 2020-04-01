c125f09fe37e2d49757c7c5d78b71324931a194a
put072 <pitoonput@gmail.com>
2020-04-01 13:21:16 +0100

    .

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,42 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Hello PUT</h1>
+    <p>Hi {{ text }}</p>
+    <label for=""> <input v-model="text" placeholder="Your name"></label>
+    <button type="submit">Sumit</button>
+    <!-- <p>{{ message }}</p> -->
+    <!-- <button v-on:click="reverseMessage">Reverse Message</button> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+
+// export default {
+//   name: 'App',
+//   components: {
+//     HelloWorld
+//   }
+// }
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+
+  data: function() {
+return {
+   text: ''
   }
+}
+
+//   data: function() {
+// return {
+//     message: 'Hello Vue.js!'
+//   };
+// },
+//   methods: {
+//     reverseMessage: function () {
+//       this.message = this.message.split('').reverse().join('')
+//     }
+//   }
 }
 </script>
 
